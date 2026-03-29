62ec62ea67b6140647383d8bb9d394a98710feab
alexandernovadev <alexsk88.dev@gmail.com>
2026-03-29 10:09:07 -0500

    Update notification settings to show banners and lists: Modify the notification handler to include options for displaying banners and lists, enhancing user engagement with reminders.

--- lib/reminders/notifications.ts
+++ lib/reminders/notifications.ts
@@ -5,7 +5,8 @@ const REMINDERS_CHANNEL_ID = 'reminders';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
   }),
